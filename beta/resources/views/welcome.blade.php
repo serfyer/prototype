83fcbc44f1fc7796c78f26d2b4ae9e2b4c46e380
Ibrahim caldea <yibra0569@gmail.com>
2019-05-16 16:21:29 -0500

    se modifico nombre de home

--- beta/resources/views/welcome.blade.php
+++ beta/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HOLA MUNDO LARAVEL
                 </div>
 
                 <div class="links">
